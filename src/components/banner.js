46234f85437be0668cb29231a12e83b342a3499e
Hardik Shali <hardik.s@accubits.com>
2023-11-07 20:08:19 +0530

    styled banner and navmenu

--- src/components/banner.js
+++ src/components/banner.js
@@ -11,8 +11,11 @@ const banner = () => {
                 <div className="row justify-content-between">
                     <div className="col-xxl-6 col-md-9 col-lg-7">
                         <div className="tf__banner_text">
-                            <h1>
-                                Hi, I'm Hardik! Creative
+                            <h1 style={{
+                                textAlign: 'left',
+                                whiteSpace: 'pre-line', // Preserve line breaks in the text
+                            }}>
+                                Hi, I'm Hardik! Creative 
                                 <span className="cd-headline rotate-1">
                                     <span className="cd-words-wrapper">
                                         <b className="is-visible">Developer</b>
@@ -20,7 +23,10 @@ const banner = () => {
                                     </span>
                                 </span>
                             </h1>
-                            <p>
+                            <p style={{
+                                textAlign: 'left',
+                                whiteSpace: 'pre-line', // Preserve line breaks in the text
+                            }}>
                                 Hello, I'm Hardik, a dedicated front-end developer with a passion for creating user-centric digital solutions. I blend creativity with technical prowess to craft aesthetically pleasing, functionally robust websites. Recently, I've delved into the world of data, seeking insights to enhance user experiences. Beyond the digital realm, music, sports, and poetry fuel my creativity. Let's shape the future of digital experiences together.
                             </p>
                             <ul>
